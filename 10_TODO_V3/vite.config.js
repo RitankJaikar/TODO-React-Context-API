f793dc811f593f2a73c99b2fb6897c39640899ff
RitankJaikar <ritank.jaikar@gmail.com>
2024-09-28 20:34:46 +0530

    Deploy

--- 10_TODO_V3/vite.config.js
+++ 10_TODO_V3/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/TODO-React-Context-API/10_TODO_V3/",
-  plugins: [react()],
-  build: {
-    outDir: 'dist/10_TODO_V3', // specify the output directory
-  },
+  base: "/TODO-React-Context-API/",
+  plugins: [react()]
 })
